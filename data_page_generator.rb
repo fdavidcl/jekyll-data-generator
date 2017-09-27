803734fbe8d9efbaf0ec8a1c9f5ca0d61b7d82d3
David Charte <fdavidcl@protonmail.com>
2017-09-27 02:35:22 +0200

    A page for each data file instead of record

--- data_page_generator.rb
+++ data_page_generator.rb
@@ -1,25 +1,13 @@
+# coding: utf-8
 # Generate pages from individual records in yml files
 # (c) 2014-2016 Adolfo Villafiorita
+# (c) 2017 David Charte
 # Distributed under the conditions of the MIT License
 
 module Jekyll
 
-  module Sanitizer
-    # strip characters and whitespace to create valid filenames, also lowercase
-    def sanitize_filename(name)
-      if(name.is_a? Integer)
-        return name.to_s
-      end
-      return name.tr(
-  "ÀÁÂÃÄÅàáâãäåĀāĂăĄąÇçĆćĈĉĊċČčÐðĎďĐđÈÉÊËèéêëĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħÌÍÎÏìíîïĨĩĪīĬĭĮįİıĴĵĶķĸĹĺĻļĽľĿŀŁłÑñŃńŅņŇňŉŊŋÑñÒÓÔÕÖØòóôõöøŌōŎŏŐőŔŕŖŗŘřŚśŜŝŞşŠšſŢţŤťŦŧÙÚÛÜùúûüŨũŪūŬŭŮůŰűŲųŴŵÝýÿŶŷŸŹźŻżŽž",
-  "AAAAAAaaaaaaAaAaAaCcCcCcCcCcDdDdDdEEEEeeeeEeEeEeEeEeGgGgGgGgHhHhIIIIiiiiIiIiIiIiIiJjKkkLlLlLlLlLlNnNnNnNnnNnNnOOOOOOooooooOoOoOoRrRrRrSsSsSsSssTtTtTtUUUUuuuuUuUuUuUuUuUuWwYyyYyYZzZzZz"
-).downcase.strip.gsub(' ', '-').gsub(/[^\w.-]/, '')
-    end
-  end
-
   # this class is used to tell Jekyll to generate a page
   class DataPage < Page
-    include Sanitizer
 
     # - site and base are copied from other plugins: to be honest, I am not sure what they do
     #
@@ -29,7 +17,7 @@ module Jekyll
     # - `name` is the key in `data` which determines the output filename
     # - `template` is the name of the template for generating the page
     # - `extension` is the extension for the generated file
-    def initialize(site, base, index_files, dir, data, name, template, extension)
+    def initialize(site, base, index_files, dir, data, data_name, template, extension)
       @site = site
       @base = base
 
@@ -38,7 +26,7 @@ module Jekyll
       #
       # the value of these variables changes according to whether we
       # want to generate named folders or not
-      filename = sanitize_filename(data[name]).to_s
+      filename = data_name.to_s
       if index_files
         @dir = dir + (index_files ? "/" + filename + "/" : "")
         @name =  "index" + "." + extension.to_s
@@ -49,80 +37,70 @@ module Jekyll
 
       self.process(@name)
       self.read_yaml(File.join(base, '_layouts'), template + ".html")
-      self.data['title'] = data[name]
+      # self.data['title'] = data[name]
+      self.data['title'] = data_name
       # add all the information defined in _data for the current record to the
       # current page (so that we can access it with liquid tags)
-      self.data.merge!(data)
+      self.data['data'] = data
     end
   end
 
   class DataPagesGenerator < Generator
     safe true
 
-    # generate loops over _config.yml/page_gen invoking the DataPage
-    # constructor for each record for which we want to generate a page
+    # generate retrieves configuration _config.yml/page_gen invoking the DataPage
+    # constructor for each data file for which we want to generate a page
 
     def generate(site)
       # page_gen_dirs determines whether we want to generate index pages
       # (name/index.html) or standard files (name.html). This information
       # is passed to the DataPage constructor, which sets the @dir variable
       # as required by this directive
-      index_files = site.config['page_gen-dirs'] == true
+      puts "debug"
+      index_files = site.config['page_gen-dirs']
+      index_files = true if index_files.nil?
 
-      # data contains the specification of the data for which we want to generate
+      # config contains the specification of the data for which we want to generate
       # the pages (look at the README file for its specification)
-      data = site.config['page_gen']
-      if data
-        data.each do |data_spec|
-          template = data_spec['template'] || data_spec['data']
-          name = data_spec['name']
-          dir = data_spec['dir'] || data_spec['data']
-          extension = data_spec['extension'] || "html"
+      config = site.config['data_gen']
 
-          if site.layouts.key? template
-            # records is the list of records defined in _data.yml
-            # for which we want to generate different pages
-            records = nil
-            data_spec['data'].split('.').each do |level|
-              if records.nil?
-                records = site.data[level]
-              else
-                records = records[level]
-              end
-            end
-            records = records.select { |r| r[data_spec['filter']] } if data_spec['filter']
-            records.each do |record|
-              site.pages << DataPage.new(site, site.source, index_files, dir, record, name, template, extension)
-            end
-          else
-            puts "error. could not find template #{template}" if not site.layouts.key? template
-          end
+      # default configuration: get all data files, use the 'data_page.html' template,
+      # output to /data
+      path = nil
+      template = 'data_page'
+      dir = 'data'
+      
+      
+      if config
+        path = config['path'] || path
+        template = config['template'] || template
+        dir = config['dir'] || dir
+      end
+
+      if site.layouts.key? template
+        data_files = path.nil? ? site.data : site.data[path]
+
+        data_files.each do |name, record|
+          site.pages << DataPage.new(site, site.source, index_files, dir, record, name, template, "html")
         end
+      else
+        puts "DataPageGenerator error. could not find template #{template}"
       end
     end
   end
 
   module DataPageLinkGenerator
-    include Sanitizer
-
     # use it like this: {{input | datapage_url: dir}}
     # to generate a link to a data_page.
     #
     # the filter is smart enough to generate different link styles
-    # according to the data_page-dirs directive ...
-    #
-    # ... however, the filter is not smart enough to support different
-    # extensions for filenames.
-    #
-    # Thus, if you use the `extension` feature of this plugin, you
-    # need to generate the links by hand
+    # according to the data_page-dirs directive 
     def datapage_url(input, dir)
       extension = Jekyll.configuration({})['page_gen-dirs'] ? '/' : '.html'
-      "#{dir}/#{sanitize_filename(input)}#{extension}"
+      "#{dir}/#{input}#{extension}"
     end
   end
 
 end
 
 Liquid::Template.register_filter(Jekyll::DataPageLinkGenerator)
-
